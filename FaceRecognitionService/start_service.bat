7f57d31a5bbd40a2b4bac19887061bd28c8cd406
vikas kumar <vikas12252@gmail.com>
2026-02-07 13:06:21 +0530

    Remove unnecessary files

--- FaceRecognitionService/start_service.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Face Recognition Service...
-echo.
-echo Make sure you have installed Python dependencies:
-echo   pip install -r requirements.txt
-echo.
-python app.py
-pause
-
